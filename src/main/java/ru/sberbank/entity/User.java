d72527cf0aa4271c5a74215be60b3de6e0b93dff
Москалев Михаил Сергеевич <MSeMoskalev@sberbank.ru>
2020-04-14 16:01:13 +0300

    Added UserDTO and CRUDservice

---
+++ src/main/java/ru/sberbank/entity/User.java
@@ -0,0 +1,89 @@
+package ru.sberbank.entity;
+
+import java.util.Objects;
+
+public class User {
+    private long id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(long id, String login, String password, String firstName, String lastName) {
+
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                login.equals(user.login) &&
+                password.equals(user.password) &&
+                firstName.equals(user.firstName) &&
+                lastName.equals(user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, firstName, lastName);
+    }
+}
